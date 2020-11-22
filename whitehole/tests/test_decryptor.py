99d566a3992f2b34b9c77c63d914ff07c957dc1e
heligp <59541291+heligp@users.noreply.github.com>
2020-11-22 16:28:12 -0500

    Update test_decryptor.py

--- whitehole/tests/test_decryptor.py
+++ whitehole/tests/test_decryptor.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 12 18:40:27 2020
+Created on Sun Nov 22 15:50:48 2020
 
-@author: HELI
+@author: QUANTMOON
 """
 
-import whitehole as wh
+from whitehole.decryptor import Decryptor
 
-dcrypt= wh.Decryptor(repo_path= 'C:/Users/HELI/data/zarr/', 
-          symbol='FB', 
-          date="2018-12-31", 
-          full_day=True, 
-          save=True,
-          storage_path='C:/Users/HELI/data/csvdata/')
+repo_path = '' ## Insert the stock repository path here
+symbol = '' ## Insert a stock from repo_path
 
-dcrypt.run_decryptor()
+dc = Decryptor(repo_path,
+                 symbol,
+                 date = None, ## If just 1 day is needed
+                 start_date = '2020-08-03', 
+                 end_date = '2020-08-05', 
+                 save = True, 
+                 dataframe = True,
+                 storage_path = '', ## The path where the information will be saved
+                 global_result = False)
 
+df = dc.run_decryptor()
+
+print(df)
 
